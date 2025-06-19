6fdb1f0518c0919b3ba718366f8f06f8e4b9687b
Antonov-S <anto1nov1@gmail.com>
2025-06-20 00:18:37 +0300

    Updated auth views (sign-in and sign-up)

--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -59,7 +59,7 @@ export const SignInView = () => {
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-5">
-      <div className="bg-[#F4F4F0], h-screen w-full lg:col-span-3 overflow-y-auto">
+      <div className="bg-[#F4F4F0] h-screen w-full lg:col-span-3 overflow-y-auto">
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -66,7 +66,7 @@ export const SignUpView = () => {
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-5">
-      <div className="bg-[#F4F4F0], h-screen w-full lg:col-span-3 overflow-y-auto">
+      <div className="bg-[#F4F4F0] h-screen w-full lg:col-span-3 overflow-y-auto">
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
