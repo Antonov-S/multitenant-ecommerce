7f23268c1c6473b65611b0932ce9e1c02b7cd769
Antonov-S <anto1nov1@gmail.com>
2025-06-22 01:53:34 +0300

    10: category pages

---
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -0,0 +1,16 @@
+interface PageProps {
+  params: Promise<{ category: string; subcategory: string }>;
+}
+
+const Page = async ({ params }: PageProps) => {
+  const { category, subcategory } = await params;
+
+  return (
+    <div>
+      Category: {category} <br />
+      Subcategory: {subcategory || "No subcategories"}
+    </div>
+  );
+};
+
+export default Page;
--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,10 +1,14 @@
 import { Suspense } from "react";
-import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-import { Navbar } from "./navbar";
-import { Footer } from "./footer";
-import { SearshFilters, SearshFiltersSkeleton } from "./search-filters";
+import { getQueryClient, trpc } from "@/trpc/server";
+
+import { Navbar } from "@/modules/home/ui/components/navbar";
+import { Footer } from "@/modules/home/ui/components/footer";
+import {
+  SearshFilters,
+  SearshFiltersSkeleton
+} from "@/modules/home/ui/components/search-filters";
 
 interface Props {
   children: React.ReactNode;
