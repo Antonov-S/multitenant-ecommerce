c90df11f4365d4f5d866c353c1eec542981f9b45
Antonov-S <anto1nov1@gmail.com>
2025-07-25 04:08:11 +0300

    27: subdomain rewrites

--- src/modules/checkout/server/procedures.ts
+++ src/modules/checkout/server/procedures.ts
@@ -13,6 +13,7 @@ import {
 } from "@/trpc/init";
 
 import { PLATFORM_FEE_PERCENTAGE } from "@/constants";
+import { generateTenantURL } from "@/lib/utils";
 
 export const checkoutRouter = createTRPCRouter({
   verify: protectedProcedure.mutation(async ({ ctx }) => {
@@ -152,11 +153,13 @@ export const checkoutRouter = createTRPCRouter({
         totalAmaunt * (PLATFORM_FEE_PERCENTAGE / 100)
       );
 
+      const domain = generateTenantURL(input.tenantSlug);
+
       const checkout = await stripe.checkout.sessions.create(
         {
           customer_email: ctx.session.user.email,
-          success_url: `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${input.tenantSlug}/checkout?success=true`,
-          cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${input.tenantSlug}/checkout?cancel=true`,
+          success_url: `${domain}/checkout?success=true`,
+          cancel_url: `${domain}/checkout?cancel=true`,
           mode: "payment",
           line_items: lineItems,
           invoice_creation: {
